a7709b28319e01df6f8676ba8b02495948564b67
RamyEl-shahedy <151165198+ramy23149@users.noreply.github.com>
2024-04-02 02:41:50 +0200

    Delete android directory

--- android/app/src/main/kotlin/com/example/food_delivery_app/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.food_delivery_app
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
